6ac254278c036128b1f03e1cc576a28a668633f3
akatlighthouse <ak.hudani@me.com>
2023-01-26 01:23:51 +0000

    W3D2

---
+++ app/demo.rb
@@ -0,0 +1,79 @@
+# # for number in (1..99)
+# #     puts number
+# # end
+
+
+# #looooops
+
+
+# group = ["A.K.", "Fred", "Bob"]
+# group 2 = [
+#     {
+#     name:'a.k.'
+#     age: 99
+# }
+
+# ]
+
+# # for name in group
+# #     if (name == "Fred")
+# #         puts 'Hi Fred!' 
+# #     end
+# #     if (name == 'Bob')
+# #         puts 'Bob does not like Fred'
+# #     end
+# # end
+
+
+# #Each Do
+# group.each do |name|
+#     puts name
+# end
+
+
+
+
+
+
+# for number in (1..100)
+#     if (number % 15 == 0)   
+#         puts 'FizzBuzz'
+#     elsif (number % 3 == 0)
+#         puts 'Fizz'
+#     elsif (number % 5 == 0)
+#         puts 'Buzz'
+#     else 
+#         puts number
+#     end
+# end
+# array = [1, 2, 3, 4]
+# array.each do |i|
+#   puts i
+# end
+
+# hash = {"a" => 1, "b" => 2, "c" => 3}
+# hash.each do |key, value|
+#   puts "The value of #{key} is #{value}"
+# end
+
+# for i in 0..5
+#     puts i
+#   end
+  
+# 5.times do |i|
+#     puts i
+#   end
+
+# x = 0
+# while x < 5
+#   puts x
+#   x += 1
+# end
+# x = 1
+# until x == 5
+#   puts x
+#   x += 1
+# end
+
+
+
